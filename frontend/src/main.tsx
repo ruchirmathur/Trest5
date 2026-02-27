8179dd8a160c78f16577447c125ede3fd32ad136
ruchirmathur <ruchirmat@gmail.com>
2026-02-27 12:47:27 -0800

    Add frontend/src/main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+import App from './App'
+import { store } from './store/store'
+import theme from './theme'
+import './index.css'
+
+const root = createRoot(document.getElementById('root')!)
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)
